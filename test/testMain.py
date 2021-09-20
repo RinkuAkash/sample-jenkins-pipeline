20773c26c4706d3b81fdfb6e6fe0a5778fad09e5
Bolliwar Akash <akashrinku2@outlook.com>
2021-09-20 18:22:54 +0530

    Update testMain.py

--- test/testMain.py
+++ test/testMain.py
@@ -5,15 +5,15 @@ from main import create_data, filter_experience, avg_salary
 
 class TestMainMethods(unittest.TestCase):
 
-    def __init_(self):
+    def setUp(self):
         self.spark = pyspark.sql.SparkSession.builder.appName("testcases").getOrCreate()
         self.employees_data = create_data(self.spark)
 
     def test_filter_experience(self):
-        self.assertEqual(3, avg_salary(self.employees_data))
+        self.assertEqual(3, filter_experience(self.employees_data).select("experience").count())
 
     def test_avg_salary(self):
-        selft.asserttEqual(2, avg_salary(self.employees_data).count())
+        self.assertEqual(2, avg_salary(self.employees_data).select("avg(salary)").count())
 
 
 if __name__ == "__main__":
